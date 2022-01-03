6309b47604291f64f8ab176b090fc559888dcbe0
Mario Miranda <mario_chan15@hotmail.com>
2022-01-03 13:52:58 -0500

    Database migrations

--- RestWithASPNET/RestWithASPNET/Startup.cs
+++ RestWithASPNET/RestWithASPNET/Startup.cs
@@ -9,26 +9,39 @@ using RestWithASPNET.Business.Implementations;
 using RestWithASPNET.Model.Context;
 using RestWithASPNET.Repository;
 using RestWithASPNET.Repository.Implementations;
+using Serilog;
+using System;
+using System.Collections.Generic;
 
 namespace RestWithASPNET
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
-        }
+            Environment = environment;
 
-        public IConfiguration Configuration { get; }
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
 
             services.AddApiVersioning();
 
@@ -55,6 +68,25 @@ namespace RestWithASPNET
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
         }
     }
 }
---
+++ RestWithASPNET/RestWithASPNET/db/dataset/v2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+	(1, 'Mario', 'Miranda', '770-201 Jacques-Berthiaume, Québec, QC', 'Male'),
+	(2, 'Cristina', 'Chan', '770-201 Jacques-Berthiaume, Québec, QC', 'Female'),
+	(3, 'Ozzy', 'Osbourne', 'Birmingham, UK', 'Male'),
+	(4, 'Lemmy', 'Kilmister', 'Burslem, UK', 'Male');
